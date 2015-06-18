bbf5fd34ce8ef64c045efb847acade0a4217b299
skultimate5 <sck42@case.edu>
2015-06-18 17:07:46 -0400

    Initial Project

---
+++ DataWebsite/App/app.js
@@ -0,0 +1,14 @@
+﻿var app = angular.module('myApp', ['ngRoute', 'kendo.directives'])
+.config(['$routeProvider', function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+          templateUrl: 'index.html',
+          controller: 'dataController'
+      })
+      .otherwise({
+          redirectTo: '/'
+      });
+}])
+.controller('mainController', function ($scope) {
+    $scope.message = "Main Content";
+});;
